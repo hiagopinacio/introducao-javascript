0bef54734f12e626227bd9dadc91465e840cb4e7
Hiago Paiva Inácio <hiagopinacio@gmail.com>
2019-12-20 10:23:49 -0300

    iterando um array com for

--- js/principal.js
+++ js/principal.js
@@ -5,39 +5,40 @@ var titulo = document.querySelector(".titulo")
 titulo.textContent = "Aparecida Nutricionista"
 
 var pacientes = document.querySelectorAll(".paciente")
-console.log(pacientes)
+
+for (var i = 0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i]
+
+    var tdPeso = paciente.querySelector(".info-peso")
+    var peso = tdPeso.textContent
+    var pesoEhValido = true
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido")
+        tdPeso.textContent = "inválido"
+        pesoEhValido = false
+    }
 
 
-var paciente = document.querySelector("#primeiro-paciente")
+    var tdAltura = paciente.querySelector(".info-altura")
+    var altura = tdAltura.textContent
+    var alturaEhValida = true
 
-var tdPeso = paciente.querySelector(".info-peso")
-var peso = tdPeso.textContent
-var pesoEhValido = true
-
-if (peso <= 0 || peso >= 1000) {
-    console.log("Peso inválido")
-    tdPeso.textContent = "inválido"
-    pesoEhValido = false
-}
+    if (altura <= 0 || altura >= 3.0) {
+        console.log("Altura inválida")
+        tdAltura.textContent = "inválido"
+        alturaEhValida = false
+    }
 
 
-var tdAltura = paciente.querySelector(".info-altura")
-var altura = tdAltura.textContent
-var alturaEhValida = true
+    var tdIMC = paciente.querySelector(".info-imc")
+    tdIMC.textContent = "ERRO"
 
-if (altura <= 0 || altura >= 3.0) {
-    console.log("Altura inválida")
-    tdAltura.textContent = "inválido"
-    alturaEhValida = false
-}
-
-
-var tdIMC = paciente.querySelector(".info-imc")
-tdIMC.textContent = "ERRO"
-
-if (pesoEhValido && alturaEhValida) {
-    var imc = peso / (altura ** altura)
-    tdIMC.textContent = imc
+    if (pesoEhValido && alturaEhValida) {
+        var imc = peso / (altura ** altura)
+        tdIMC.textContent = imc
+    }
 }
 
 
