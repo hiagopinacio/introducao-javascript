3afa57d8527ef2533a5b87ea77c7551bfb77104f
Hiago Paiva Inácio <hiagopinacio@gmail.com>
2019-12-20 16:31:21 -0300

    Criando elementos html através do javascrip e adicionando estes elementos na página.

--- js/principal.js
+++ js/principal.js
@@ -47,6 +47,34 @@ var botaoFormulario = document.querySelector("#adicionar-paciente")
 
 botaoFormulario.addEventListener("click", function (event) {
     event.preventDefault()
-    console.log("botao clicado")
+
+    var formAdiciona = document.querySelector("#form-adiciona")
+
+    var tabela = document.querySelector("#tabela-pacientes")
+
+    var pacienteTr = document.createElement("tr")
+    pacienteTr.classList.add("paciente")
+    tabela.appendChild(pacienteTr)
+
+    var nomeTd = document.createElement("td")
+    nomeTd.textContent = formAdiciona.nome.value
+    nomeTd.classList.add("info-nome")
+    pacienteTr.appendChild(nomeTd)
+
+    var pesoTd = document.createElement("td")
+    pesoTd.textContent = formAdiciona.peso.value
+    pesoTd.classList.add("info-peso")
+    pacienteTr.appendChild(pesoTd)
+
+    var alturaTd = document.createElement("td")
+    alturaTd.textContent = formAdiciona.altura.value
+    alturaTd.classList.add("info-altura")
+    pacienteTr.appendChild(alturaTd)
+
+    var gorduraTd = document.createElement("td")
+    gorduraTd.textContent = formAdiciona.gordura.value
+    gorduraTd.classList.add("info-gordura")
+    pacienteTr.appendChild(gorduraTd)
+
 })
 
