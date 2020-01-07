eac46609decbf592961834b8335b74595dfe0617
Hiago Paiva Inácio <hiagopinacio@gmail.com>
2020-01-07 17:25:19 -0300

    XMLHttpresponse: Tratando possíveis erros de requisição.

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -8,15 +8,22 @@ btnBuscarPacientes.addEventListener("click", function () {
 
     xhr.addEventListener("load", function () {
 
-        var resposta = xhr.responseText //string com resultado da requisição
-        var pacientesImportados = JSON.parse(resposta)
+        if (xhr.status == 200) { //o código 200 diz que a requisição foi bem sucedida
+            document.querySelector("#erro-buscar-paciente").innerHTML = ""
+            var resposta = xhr.responseText //string com resultado da requisição
+            var pacientesImportados = JSON.parse(resposta)
 
-        console.log(pacientesImportados); //transforma a string recebida em um array com objetos js
+            console.log(pacientesImportados); //transforma a string recebida em um array com objetos js
 
-        pacientesImportados.forEach(function (paciente) {
+            pacientesImportados.forEach(function (paciente) {
 
-            adicionaPaciente(paciente)
-        })
+                adicionaPaciente(paciente)
+            })
+        } else {
+            document.querySelector("#erro-buscar-paciente").textContent = "erro ao carregar lista"
+            console.log("Erro na requisição:");
+            console.log(xhr.statusText)
+        }
 
     })
     xhr.send()
